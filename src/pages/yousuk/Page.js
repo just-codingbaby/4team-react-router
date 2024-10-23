b8bf628a115c4c919f4170ab2e3882016d2f0cb5
Yousuk <yousuk88@gmail.com>
2024-10-23 18:14:07 +0900

    add: yousuk page

---
+++ src/pages/yousuk/Page.js
@@ -0,0 +1,11 @@
+import { Flex } from "../../common/components/Flex";
+import "./Page.css";
+
+export function TaejinIntroPage() {
+  return (
+    <Flex.Column>
+      <h1 className="text">내 이름은 유석</h1>
+      <h1>고양시민이죠</h1>
+    </Flex.Column>
+  );
+}
